59d1ee5a684759d5833e49f1d92c3cb3d92e74fb
JHJ4012 <wjdguswo4012@gmail.com>
2019-12-06 21:16:52 +0900

    df

--- resources/js/components/QnAList.vue
+++ resources/js/components/QnAList.vue
@@ -18,7 +18,7 @@
 				</tr>
 			</thead>
 			<tbody>
-				<tr v-for = "qna in qnas.data" @click="clickQnA(qna)">
+				<tr v-for = "qna in qnas.data" v-bind:key="qna" @click="clickQnA(qna)">
 					<td>{{ number + (qnas.data.indexOf(qna)+1 )}}</td>
 					<td>{{ qna.title }}</td>
 					<td>{{ qna.user_id }}</td>
